f002fc0f8675c029fcb49b93e8c4d6c1dff63137
imane <imane.lekhzoumy@outlook.com>
2017-04-21 19:29:05 +0100

    homepage

--- modules/system/page.tpl.php
+++ modules/system/page.tpl.php
@@ -77,6 +77,8 @@
 
     <div id="header"><div class="section clearfix">
 
+      <?php print $style; ?>
+
       <?php if ($logo): ?>
         <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo">
           <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
