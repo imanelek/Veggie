33f34a48e4cba603490321d5de2036dd85fbdf17
imanelek <imane.lekhzoumy@gmail.com>
2017-07-20 11:17:29 +0200

    feature/25000 j'ai fait un test

--- sites/all/themes/restaurant/template.php
+++ sites/all/themes/restaurant/template.php
@@ -8,4 +8,5 @@ function restaurant_form_alter(&$form, &$form_state, $form_id) {
   if ($form_id == 'search_block_form') {
    $form['search_block_form']['#attributes']['placeholder'] = 'SEARCH';
     }
+    //test.
 }
